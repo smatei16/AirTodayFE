f00fb0dcaa7aae19fcbc6c181f0ee7f4e2c651bb
smatei16 <55893625+smatei16@users.noreply.github.com>
2023-12-27 15:08:06 +0200

    Added working map with api key

--- air-today/src/components/map/MapComponent.jsx
+++ air-today/src/components/map/MapComponent.jsx
@@ -1,37 +1,28 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { loadModules } from 'esri-loader';
 
 const MapComponent = () => {
-    const mapViewRef = useRef(null);
+  useEffect(() => {
+    loadModules(['esri/config', 'esri/Map', 'esri/views/MapView'], { css: true })
+      .then(([esriConfig, Map, MapView]) => {
+        // Adaugă cheia API
+        esriConfig.apiKey = 'AAPK6f8e89b5cf0a47948d9b73025c20fe1eX6JYT1csqWTHrDRabpXSs8zaKl_2dCXLhR7MZ5fU15PPlDnhkVzjbCqqPXqCeJtR';
 
-    useEffect(() => {
-      const loadMap = async () => {
-        
-        const [Map, MapView] = await loadModules(['esri/Map', 'esri/views/MapView']);
-        const mapProperties = {
-            basemap: 'streets',
-        };
-        const map = new Map(mapProperties);
+        const map = new Map({
+          basemap: 'streets'
+        });
 
-        const mapViewProperties = {
-            container: mapViewRef.current,
-            center: [26.096306, 44.439663], // Center of Bucharest
-            zoom: 10,
-            map: map,
-          };
-        const view = new MapView(mapViewProperties);
-  
-        view.container = 'mapContainer';
-  
-        view.map = map;
-      };
-  
-      loadMap();
-    }, []);
-  
-    return (
-      <div id="mapContainer" style={{ width: '100%', height: '1080px' }}></div>
-    );
-  };
-  
-  export default MapComponent;
+        const view = new MapView({
+          container: 'mapContainer',
+          map: map,
+          center: [26.096306, 44.439663],
+          zoom: 10
+        });
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return <div id="mapContainer" style={{ height: '100vh' }}></div>;
+};
+
+export default MapComponent;
